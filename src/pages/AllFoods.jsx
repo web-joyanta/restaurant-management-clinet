d4e6fb9e52cd8392029aff14ced5de24d180696c
web-joyanta <web.joyanta@gmail.com>
2025-11-25 23:40:27 +0600

    Implement pagination for food items and enhance search functionality

--- src/pages/AllFoods.jsx
+++ src/pages/AllFoods.jsx
@@ -1,16 +1,32 @@
 import { useQuery } from "@tanstack/react-query";
-import useAxios from "../hooks/useAxios";
+import { useState } from "react";
 import FoodCard from "../components/FoodCard";
+import useAxios from "../hooks/useAxios";
 
 const AllFoods = () => {
     const categories = ["Starter", "Main Course", "Fast Food", "Japanese Cuisine", "Italian Cuisine", "Traditional", "Salad", "Dessert", "Beverage", "Noodles", "Side"];
+
     const axiosInstance = useAxios();
+    const [itemsPerPage] = useState(6);
+    const [currentPage, setCurrentPage] = useState(1);
+
+    // foods count
+    const { data: foodsCount } = useQuery({
+        queryKey: ['foods-count'],
+        queryFn: async () => {
+            const res = await axiosInstance.get('/foods-count');
+            return res.data;
+        }
+    });
+
+    const totalPages = Math.ceil((foodsCount?.count || 0) / itemsPerPage);
+    const pages = [...Array(totalPages).keys()].map(num => num + 1);
 
     // all data get
     const { isPending, data } = useQuery({
-        queryKey: ['foods'],
+        queryKey: ['foods', currentPage, itemsPerPage],
         queryFn: async () => {
-            const res = await axiosInstance.get('/foods');
+            const res = await axiosInstance.get(`/foods?page=${currentPage}&size=${itemsPerPage}`);
             return res.data;
         }
     });
@@ -24,6 +40,7 @@ const AllFoods = () => {
         )
     }
 
+    // 
     return (
         <div className="bg-gray-50 pb-20">
             <div className="bg-custom-orange text-white text-center py-20">
@@ -36,14 +53,13 @@ const AllFoods = () => {
                 <div className="bg-white rounded-xl shadow-md p-6">
                     <div className="flex items-center justify-between mb-4">
                         <h3 className="text-lg font-semibold text-gray-800">Filter & Search</h3>
-                        <button
-                            className="btn btn-ghost btn-sm text-gray-600 hover:text-custom-orange"
-                        >
-                            Reset All
+                        <button className="btn bg-custom-orange text-white">
+                            Reset
                         </button>
+
                     </div>
 
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
                         {/* Category Filter */}
                         <div className="form-control">
                             <label className="label">
@@ -60,14 +76,14 @@ const AllFoods = () => {
                         </div>
 
                         {/* Search Field */}
-                        <div className="form-control">
-                            <label className="label">
+                        <div className="form-control lg:col-span-2">
+                            <label className="label flex lg:justify-center">
                                 <span className="label-text">Search</span>
                             </label>
                             <input
                                 type="text"
                                 placeholder="Search foods by name..."
-                                className="input input-bordered w-full focus:border-custom-orange"
+                                className="input input-bordered w-full lg:border-2 lg:border-custom-orange  lg:rounded-4xl"
                             />
                         </div>
 
@@ -90,6 +106,27 @@ const AllFoods = () => {
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 mt-7">
                     {data.map(food => <FoodCard key={food._id} food={food} ></FoodCard>)}
                 </div>
+                {/* navigation start*/}
+                <div className="flex mt-9 justify-center">
+                    <button onClick={() => setCurrentPage(currentPage - 1)} disabled={currentPage === 1} className="flex shadow items-center justify-center px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-white rounded-md rtl:-scale-x-100 dark:bg-gray-800 dark:text-gray-200 hover:bg-custom-orange dark:hover:bg-custom-orange hover:text-white dark:hover:text-gray-200">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
+                            <path fillRule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clipRule="evenodd" />
+                        </svg>
+                    </button>
+
+                    {pages.map(page => (
+                        <button onClick={() => setCurrentPage(page)} key={page} className={`hidden shadow px-4 py-2 mx-1 text-custom-orange transition-colors duration-300 transform  rounded-md sm:inline dark:bg-gray-800 dark:text-gray-200 ${currentPage === page ? 'bg-custom-orange text-white' : 'bg-white'} hover:bg-custom-orange dark:hover:bg-custom-orange hover:text-white dark:hover:text-gray-200`}>
+                            {page}
+                        </button>
+                    ))}
+
+                    <button onClick={() => setCurrentPage(currentPage + 1)} disabled={currentPage === totalPages} className="flex shadow items-center justify-center px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-white rounded-md rtl:-scale-x-100 dark:bg-gray-800 dark:text-gray-200 hover:bg-custom-orange dark:hover:bg-custom-orange hover:text-white dark:hover:text-gray-200">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
+                            <path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" />
+                        </svg>
+                    </button>
+                </div>
+                {/* navigation end */}
             </div>
         </div>
     );
